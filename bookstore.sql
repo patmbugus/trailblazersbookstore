07fc8c92b94fd4cbf702f41512f9f561b8bd05cb
Hapsudo <catherine.abugah8@gmail.com>
2025-04-12 01:12:06 +0300

    Book Table Creation

--- bookstore.sql
+++ bookstore.sql
@@ -1,1 +1,13 @@
+-- Creating of Database
+CREATE DATABASE BookStore;
 
+-- Creating Book Table
+CREATE TABLE Book (
+book_id INT PRIMARY KEY,
+title VARCHAR(50),
+publisher_id INT, 
+language_id INT,
+published_year VARCHAR(50),
+unit_price DECIMAL(10,2),
+stock_quantity INT
+);
